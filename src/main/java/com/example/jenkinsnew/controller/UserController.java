74882d59f85e98361f4621efd9f285970db98140
Jayesh Mahajan <jayesh.mahajan@atyeti.com>
2021-03-30 09:43:42 +0530

    project added

---
+++ src/main/java/com/example/jenkinsnew/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsnew.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    UserController(){
+
+    }
+
+    @GetMapping("/list")
+    public String getList(){
+        return "Hii Jayesh" ;
+    }
+
+}
